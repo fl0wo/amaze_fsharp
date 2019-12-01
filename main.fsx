163cda1a0f903be5833e15f2a424f44720126fa6
fl0wo <sabaniflorian@gmail.com>
2019-12-01 23:24:17 +0100

    Fluid controls

--- main.fsx
+++ main.fsx
@@ -36,6 +36,7 @@ type ColorEnum =
     | Bloccato = 1
     | Aperto = 0
     | User = 3
+    | End = 4
 
 
 (*
@@ -56,35 +57,49 @@ type Player(x: int, y: int) =
     member this.x = _x
     member this.y = _y
 
-    member this.goUp = _y <- (_y - 1)
-    member this.goDown = _y <- (_y + 1)
-    member this.goLeft = _x <- (_x - 1)
-    member this.goRight = _x <- (_x + 1)
+    member this.isLegal (x, y) = (x > 0 && y > 0)
+
+
+    member this.goUp = _y <- (_y - 1);this.isLegal(x,y);
+    member this.goDown = _y <- (_y + 1);this.isLegal(x,y);
+    member this.goLeft = _x <- (_x - 1);this.isLegal(x,y);
+    member this.goRight = _x <- (_x + 1);this.isLegal(x,y);
 
 type Mappa(r: int, c: int) =
     let _mappa: array<array<int>> = Utils.initMatrix r c (int ColorEnum.Bloccato)
 
+    let mutable _end: int * int = (0,0)
+
+    member this.finish = _end
     member this.mappa = _mappa
 
     member this.r = r
     member this.c = c
 
+    member this.setEnd (x,y) = 
+        _end <- (x,y);
+        _mappa.[x].[y] <- (int ColorEnum.End)
+
+    member this.isLegal (x, y) = (x > 0 && x < (c - 1) && y > 0 && y < (r - 1))
+
+    member this.canGo y x =     
+        this.isLegal(x,y) && _mappa.[x].[y] <> (int ColorEnum.Bloccato);
+
     // Generates the labirinth
     member this.initLabirinto =
-        let isLegal (x, y) = (x > 0 && x < (c - 1) && y > 0 && y < (r - 1))
 
         let frontier (x, y) =
             [ x - 2, y
               x + 2, y
               x, y - 2
               x, y + 2 ]
-            |> List.filter (fun (x, y) -> isLegal (x, y) && _mappa.[x].[y] = (int ColorEnum.Bloccato))
+            |> List.filter (fun (x, y) -> this.isLegal (x, y) && _mappa.[x].[y] = (int ColorEnum.Bloccato))
         let neighbor (x, y) =
             [ x - 2, y
               x + 2, y
               x, y - 2
               x, y + 2 ]
-            |> List.filter (fun (x, y) -> isLegal (x, y) && _mappa.[x].[y] = (int ColorEnum.Aperto))
+            |> List.filter (fun (x, y) -> this.isLegal (x, y) && _mappa.[x].[y] = (int ColorEnum.Aperto))
 
         let randomCell() = (1 + rand.Next(c - 1)), (1 + rand.Next(r - 1))
 
@@ -146,7 +161,26 @@ type Mappa(r: int, c: int) =
         clone.[tUser.y].[tUser.x] <- (int ColorEnum.User)
         clone
 
-    member randPosition = 
+    // Sempre un quadratino vuoto circondato da almeno 3 muretti
+    member this.randSpawn =
+        let mutable coolSpawns = []
+        let mutable nCoolSpawns = 0
+        for y in 1 .. (r - 1) do
+            for x in 1 .. (c - 1) do
+                if (_mappa.[y].[x] = (int ColorEnum.Aperto)) then
+                    let mutable nAdiacentWalls = 0
+                    if (_mappa.[y - 1].[x] = (int ColorEnum.Bloccato)) then nAdiacentWalls <- nAdiacentWalls + 1
+                    if (_mappa.[y + 1].[x] = (int ColorEnum.Bloccato)) then nAdiacentWalls <- nAdiacentWalls + 1
+                    if (_mappa.[y].[x - 1] = (int ColorEnum.Bloccato)) then nAdiacentWalls <- nAdiacentWalls + 1
+                    if (_mappa.[y].[x + 1] = (int ColorEnum.Bloccato)) then nAdiacentWalls <- nAdiacentWalls + 1
+
+                    if nAdiacentWalls>2 then
+                        coolSpawns <- coolSpawns @ [ (y, x) ]
+                        nCoolSpawns <- nCoolSpawns + 1
+
+        if nCoolSpawns = 0 then (0, 0)
+        else coolSpawns.[rand.Next(nCoolSpawns)]
+
 
 
 (*
@@ -208,28 +242,29 @@ module UtilsView =
 
 
 let mappa: Mappa = new Mappa(21, 21)
-let user: Player = new Player(3, 3)
 
 mappa.initLabirinto
 
+let (startY, startX) = mappa.randSpawn
+let user: Player = new Player(startX, startY)
+
+let (endX, endY) = mappa.randSpawn
+mappa.setEnd (endX,endY);
 
 module Control =
 
     let onKey (k: string): bool =
         match k with
-        | "LeftArrow" ->
-            (user.goLeft
-             true)
-        | "RightArrow" ->
-            (user.goRight
-             true)
+        | "LeftArrow" -> (mappa.canGo (user.x-1) (user.y)) && user.goLeft;
+
+        | "RightArrow" -> mappa.canGo (user.x+1) (user.y) && user.goRight;
+
         | "Spacebar" -> (false)
-        | "DownArrow" ->
-            (user.goDown
-             true)
-        | "UpArrow" ->
-            (user.goUp
-             true)
+
+        | "DownArrow" -> mappa.canGo user.x (user.y+1) && user.goDown;
+
+        | "UpArrow" -> mappa.canGo user.x (user.y-1) && user.goUp;
+
         | _ -> (false)
 
 let rec reactiveKey() =
